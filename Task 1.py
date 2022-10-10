9c1f6423a8e1835e0953d59f7753d0cf587d7bd6
DanielRafik <daniabdelmesih00@gmail.com>
2022-10-11 01:35:37 +0200

    sx

--- Task 1.py
+++ Task 1.py
@@ -1,13 +1,38 @@
 from PIL import Image
 import numpy as np
 import os
+import sys
+import pydicom as dicom
+import matplotlib.pylab as plt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt5 import uic, QtGui, QtCore
+from PyQt5.QtGui import QPixmap
 
-Image_jpg = Image.open('coin.jpg')
-Image_bmp=Image.open('sample_640×426.bmp')
-Image_bmp.show()
-Image_jpg.show()
-print(Image_jpg.format)
-print(Image_jpg.mode)
-print(Image_jpg.width)
-print(Image_jpg.height)
+
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi(r"task11.ui", self)
+        self.Browse_Button.clicked.connect(self.Browse)
+        self.show()
+
+    def Browse(self):
+        global file_name
+        file_name=QFileDialog.getOpenFileName(self, "Browse Image", "../", ".dcm;;" " *.bmp;;" ".jpeg;;" )
+        if file_name[0].endswith(".bmp") | file_name[0].endswith(".jpg"):
+            print(file_name)
+            self.Normal_Image=QPixmap(file_name)
+            self.Image_label.setPixmap(self.Normal_Image)
+
+
+
+
+
+
 print(os.stat('coin.jpg').st_size)
+#def calculate_image_info(self):
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec_()
+
